379f39374aa36426055e75483139c564a2161171
Guido Landoni <Guido_Landoni@itrsa.com.ar>
2024-06-26 17:22:21 -0300

    Agrego notFoundPath

--- src/middlewares/notFoundPath.js
+++ src/middlewares/notFoundPath.js
@@ -1,5 +1,7 @@
-const notFoundPath = ()=>{
-
+const notFoundPath = (req, res, next)=>{
+    return res.status(400).json({
+        message: "NOT FOUND PATH"
+    });
 }
 
 export default notFoundPath
